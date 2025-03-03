3c1dce68b7fb0c33440ae29edff3da4a8381d82a
Idikopp90 <151070346+Idikopp90@users.noreply.github.com>
2025-03-03 15:52:31 +0000

    Update config.js

--- config.js
+++ config.js
@@ -187,7 +187,7 @@ global.APIKeys = {
 //-------------
 global.premium = [`${ownernum}`]
 //-------------
-global.sessionName = 'session.alfa'
+global.sessionName = 'session idikopp2@gmail.com 
 //-------------
 global.prefa = ['','!','.','🐦','🐤','🗿']
 //-------------
